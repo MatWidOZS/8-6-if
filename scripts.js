a08841bf42c948c58287d37085d1fa532c586a25
Mateusz Widerski <mat.widerski@gmail.com>
2018-02-28 18:54:06 +0100

    Add index, styles, scripts

---
+++ scripts.js
@@ -0,0 +1,15 @@
+var a = prompt('Give a value of "a"'),
+    b = prompt('Give a value of "b"'),
+    value;
+
+value = (a * a) - (2 * a * b) - (b * b);
+
+console.log('Wynik działania wynosi: ' + value);
+
+if (value > 0) {
+	console.log('Wynik dodatni');
+} else if (value < 0) {
+	console.log('Wynik ujemny');
+} else if (value == 0) {
+	console.log('Wynik to ' + value);
+}
